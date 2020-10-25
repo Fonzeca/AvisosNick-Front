5a3ac9cde4aa926ee95e759169dc10ebeefed0a1
Fragore <alexisfonzos_@hotmail.com>
2020-10-25 20:09:44 -0300

    Fix overflow pixels in Notice Admin

--- lib/screens/notice.dart
+++ lib/screens/notice.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nick_tecnologia_notices/utilities/constants.dart';
@@ -8,7 +7,8 @@ class Notice extends StatefulWidget {
   bool admin;
   List<String> usuariosNotificados;
 
-  Notice(String titulo, String mensaje, String autor, String fecha, bool admin, List<String> usuariosNotificados){
+  Notice(String titulo, String mensaje, String autor, String fecha, bool admin,
+      List<String> usuariosNotificados) {
     this.titulo = titulo;
     this.mensaje = mensaje;
     this.autor = autor;
@@ -19,14 +19,15 @@ class Notice extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-    if(admin){
-      return NoticeStateAdmin(titulo,mensaje,autor,fecha,usuariosNotificados);
+    if (admin) {
+      return NoticeStateAdmin(
+          titulo, mensaje, autor, fecha, usuariosNotificados);
     }
     return NoticeState(titulo, mensaje, autor, fecha);
   }
 }
 
-class NoticeStateAdmin extends State<Notice>{
+class NoticeStateAdmin extends State<Notice> {
   String titulo, mensaje, autor, fecha;
   List<String> usuariosNotificados;
 
@@ -36,105 +37,118 @@ class NoticeStateAdmin extends State<Notice>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Aviso"),
-        ),
-        body: Container(
-            width: double.infinity,
-            height: double.infinity,
-            child: Card(
-                margin: EdgeInsets.symmetric(horizontal: 25, vertical: 40),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children:<Widget> [
-                        Text(
-                          titulo,
-                          style: fTitleNoticeOpen,
-                          textAlign: TextAlign.center,
+      appBar: AppBar(
+        title: Text("Aviso"),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Card(
+          margin: EdgeInsets.symmetric(horizontal: 25, vertical: 40),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  titulo,
+                  style: fTitleNoticeOpen,
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Expanded(
+                  flex: 9,
+                  child: SingleChildScrollView(
+                    child: Container(
+                      width: double.infinity,
+                      child: Text(
+                        mensaje,
+                        textAlign: TextAlign.left,
+                      )
+                    ),
+                  ),
+                ),
+                Divider(),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: SingleChildScrollView(
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Text("Fecha de creación: "),
+                                Text(fecha, maxLines: 3,)
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Text("Escrita por: "),
+                                Text(autor)
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Text("Usuarios notificados: "),
+                                _listUsuariosNotificados()
+                              ],
+                            )
+                          ]
                         ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        Expanded(
-                          flex: 9,
-                          child: SingleChildScrollView(
-                            child: Container(
-                                width: double.infinity,
-                                child: Text(
-                                  mensaje,
-                                  textAlign: TextAlign.left,
-                                )),
-                          ),
-                        ),
-                        Divider(),
-                        Expanded(
-                          flex: 2,
-                            child:SingleChildScrollView(
-                              child:
-                              Column(
-                                  children: <Widget>[
-                                    Row(
-                                      children: <Widget>[
-                                        Text("Fecha de creación: "),
-                                    Text(fecha)
-                                  ],
-                                ),
-                                SizedBox(height: 20,),
-                                Row(
-                                  children: <Widget>[
-                                    Text("Escrita por: "),
-                                    Text(autor)
-                                  ],
-                                ),
-                                SizedBox(height: 20,),
-                                Row(
-                                  children: <Widget>[
-                                    Text("Usuarios notificados: "),
-                                    _listUsuariosNotificados()
-                                  ],
-                                )
-                              ]),)
-                        ),
-                      ]),
-                )
-            )
-        ));
+                      ),
+                    ),
+                  )
+                ),
+              ]
+            ),
+          )
+        )
+      )
+    );
   }
 
-  Widget _listUsuariosNotificados(){
-    if(usuariosNotificados == null || usuariosNotificados.isEmpty){
+  Widget _listUsuariosNotificados() {
+    if (usuariosNotificados == null || usuariosNotificados.isEmpty) {
       return Text("N/A");
     }
     return ListView.builder(
-        itemCount:usuariosNotificados.length,
-        itemBuilder: (BuildContext context,  index){
+        itemCount: usuariosNotificados.length,
+        itemBuilder: (BuildContext context, index) {
           return _itemUsuarioNotificado(usuariosNotificados[index]);
+        });
+  }
 
-        }
+  Widget _itemUsuarioNotificado(String usuario) {
+    return Card(
+      child: ListTile(
+        title: Text(usuario),
+      ),
     );
-}
-    Widget _itemUsuarioNotificado(String usuario){
-      return Card(
-        child: ListTile(
-          title: Text(usuario),
-        ),
-      );
-    }
+  }
 }
 
 class NoticeState extends State<Notice> {
   String titulo, mensaje, autor, fecha;
 
-  NoticeState(String titulo, String mensaje, String autor, String fecha){
+  NoticeState(String titulo, String mensaje, String autor, String fecha) {
     this.titulo = titulo;
     this.mensaje = mensaje;
     this.autor = autor;
     this.fecha = fecha;
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -183,12 +197,11 @@ class NoticeState extends State<Notice> {
                           Text(fecha)
                         ],
                       ),
-                      SizedBox(height: 20,),
+                      SizedBox(
+                        height: 20,
+                      ),
                       Row(
-                        children: <Widget>[
-                          Text("Escrita por: "),
-                          Text(autor)
-                        ],
+                        children: <Widget>[Text("Escrita por: "), Text(autor)],
                       )
                     ],
                   ),
