1aa3454f4f45f3aaaad0506dd769f3735bf99a55
Fragore <alexisfonzos_@hotmail.com>
2020-11-13 09:25:12 -0300

    Test

--- android/app/src/main/java/io/flutter/app/Application.java
+++ android/app/src/main/java/io/flutter/app/Application.java
@@ -1,28 +1,12 @@
 package io.flutter.app;
 
-import io.flutter.app.FlutterApplication;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
-import io.flutter.plugins.GeneratedPluginRegistrant;
-import io.flutter.plugins.firebase.auth.FlutterFirebaseAuthPlugin;
-import io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin;
-import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
-import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
-//import io.flutter.plugins.googlesignin.GoogleSignInPlugin;
 
-public class Application extends FlutterApplication implements PluginRegistrantCallback {
+public class Application extends FlutterApplication {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		FlutterFirebaseMessagingService.setPluginRegistrant(this);
 	}
 
-	@Override
-	public void registerWith(PluginRegistry registry) {
-		System.out.println("PLUGINS REGISTRADOS");
-		FirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
-		FlutterFirebaseAuthPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebase.auth.FlutterFirebaseAuthPlugin"));
-		FlutterFirebaseCorePlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin"));
-//		GoogleSignInPlugin.registerWith(registry.registrarFor("io.flutter.plugins.googlesignin.GoogleSignInPlugin"));
-	}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:nick_tecnologia_notices/screens/administrator/admin_analysis_menu.dart';
+import 'package:nick_tecnologia_notices/screens/administrator/admin_notices_menu.dart';
+import 'package:nick_tecnologia_notices/screens/administrator/admin_userTypes_menu.dart';
+import 'package:nick_tecnologia_notices/screens/administrator/admin_users_menu.dart';
+import 'package:nick_tecnologia_notices/screens/administrator/administrator_menu.dart';
+import 'package:nick_tecnologia_notices/screens/dash_board_notices.dart';
+import 'package:nick_tecnologia_notices/screens/edit_user_screen.dart';
+import 'package:nick_tecnologia_notices/screens/login_screen_v3.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,11 +19,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Holis"),
-        ),
-      ),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      debugShowCheckedModeBanner: false,
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: LoginScreen(),
+      routes: {
+      '/dashBoard' : (BuildContext context) => new DashBoardNotices(),
+      '/administrator' : (BuildContext context) => new AdministratorMenu(),
+      '/myAccount' : (BuildContext context) => new EditUser(false),
+      '/administrator/usuarios' : (BuildContext context) => new AdminMenuUsuarios(),
+      '/administrator/usuarios/editUser' : (BuildContext context) => new EditUser(true),
+      '/administrator/userTypes' : (BuildContext context) => new AdminUserTypeMenu(),
+      '/administrator/notices' : (BuildContext context) => new AdminNoticeMenu(),
+      '/administrator/analysis' : (BuildContext context) => new AdminAnalysisMenu(),
+      '/login' : (BuildContext context) => new LoginScreen(),
+    },
     );
 
   }
